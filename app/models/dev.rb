0fdc3c1ccc5b0eff21cba2e7a42b020cbe04cdf7
asher scott <asher.scott6@gmail.com>
2022-04-12 09:09:52 -0500

    commit tree 9b22140eda98b360124f366b9a05f4e418103e90

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,5 +2,14 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
+    def recieved_one? item
+        Freebie.all.find_by(item_name: item) ? true : false
+    end
 
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+        "#{freebie.item_name} now belongs to #{freebie.dev.name}!"
+        end
+    end
 end
